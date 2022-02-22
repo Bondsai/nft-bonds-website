eb0a6f3b05fd18f9b3b7c2e19870329d19f08fe7
Danielto1404 <daniilkorolev00@gmail.com>
2022-02-22 12:09:44 +0300

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import PurpleGreenButton from "./components/common/buttons/PurpleBlueButton";
 
-const App = () => (
+const App: React.FC = () => (
     <div className="min-h-screen bg-sol-gray">
         <div className="text-sol-white p-40 text-3xl font-bold">
             <PurpleGreenButton>
---
+++ src/pages/EventScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface EventScreenProps {
+
+}
+
+const EventScreen: React.FC<EventScreenProps> = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default EventScreen;
