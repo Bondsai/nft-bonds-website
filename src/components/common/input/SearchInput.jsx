da1337787652289dbbf734d5d9a972c184f5b3e1
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-02-22 18:00:42 +0300

    compact search

--- src/components/common/input/SearchInput.jsx
+++ src/components/common/input/SearchInput.jsx
@@ -3,9 +3,11 @@ import {BiSearchAlt2} from "react-icons/bi";
 
 const SearchInput = React.forwardRef((props, ref) => {
     return (
-        <div className="flex grid-rows-1 justify-center">
-            <input ref={ref} {...props}/>
-            <BiSearchAlt2 className="p-2 text-sol-white h-full w-12 hover:bg-sol-gray-light rounded-lg"/>
+        <div className="justify-center flex my-4 p-1">
+            <div className="flex justify-center bg-sol-gray-light w-auto rounded-2xl">
+                <input ref={ref} className="rounded-lg m-2 bg-sol-gray-light text-sol-white w-60" {...props}/>
+                <BiSearchAlt2 className="p-2 text-sol-white h-full w-14"/>
+            </div>
         </div>
     );
 });
--- src/pages/ExploreBonds.jsx
+++ src/pages/ExploreBonds.jsx
@@ -39,26 +39,25 @@ const ExploreBonds = () => {
     return (<div>
         <div className="text-sol-green p-40 text-3xl font-bold">Navbar</div>
         <hr className="bg-sol-white"/>
-        <div className="grid grid-rows-1 md:grid-cols-2 justify-center">
+        <div className="grid grid-rows-1 md:grid-cols-3 justify-center">
             <button
-                className="text-3xl text-sol-white justify-center text-center mx-40 p-5 hover:bg-sol-gray-light rounded-lg"
+                className="text-2xl text-sol-white justify-center text-center mx-40 p-5 hover:bg-sol-gray-light rounded-lg"
             >
                 Active Events
             </button>
             <button
-                className="text-3xl text-sol-white mx-40 p-5 text-center hover:bg-sol-gray-light rounded-lg"
+                className="text-2xl text-sol-white mx-40 p-5 text-center hover:bg-sol-gray-light rounded-lg"
             >
                 Completed Events
             </button>
+            <SearchInput
+                value={request}
+                onChange={e => setRequest(e.target.value)}
+                type='text'
+                placeholder="Search company..."
+            />
         </div>
         <hr className="bg-sol-white"/>
-        <SearchInput
-            value={request}
-            onChange={e => setRequest(e.target.value)}
-            type='text'
-            placeholder="Company name"
-            className="rounded-lg m-2"
-        />
         <div
             className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center">
             {items.map(item => <EventItem defaultIsFinished={item.value} key={item.id}>
