50ccfa5b6be63286fe989189c4468de43ac9daa2
Danielto1404 <daniilkorolev00@gmail.com>
2022-02-22 13:11:25 +0300

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 module.exports = {
     content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
     theme: {
+        fontFamily: {
+            'archivo': ['Archivo'],
+        },
+        screens: {
+            'xxs': '310px',
+            'xs': '500px',
+            'sm': '660px',
+            'md': '768px',
+            'lg': '990px',
+            'xl': '1320px',
+            '2xl': '1536px',
+
+            'max-xxs': {'max': '310px'},
+            'max-xs': {'max': '500px'},
+            'max-sm': {'max': '660px'},
+            'max-md': {'max': '768px'},
+            'max-lg': {'max': '990px'},
+            'max-xl': {'max': '1320px'},
+            'max-2xl': {'max': '1536px'},
+        },
+
         extend: {
             colors: {
                 'sol-gray': 'rgb(35, 39, 45)',
