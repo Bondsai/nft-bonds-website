bf039e13decd2017ba0481a20b8ffb6ef4cfaa08
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-02-23 19:02:31 +0300

    some fix

--- src/components/common/input/SearchInput.jsx
+++ src/components/common/input/SearchInput.jsx
@@ -4,8 +4,8 @@ import {BiSearchAlt2} from "react-icons/bi";
 const SearchInput = React.forwardRef((props, ref) => {
     return (
         <div className="justify-center flex my-4 p-1">
-            <div className="flex justify-center bg-sol-gray-button-hover w-auto rounded-2xl">
-                <input ref={ref} className="rounded-lg m-2 bg-sol-gray-button-hover text-sol-white w-36 lg:w-60" {...props}/>
+            <div className="flex justify-center bg-sol-gray-input-hover w-auto rounded-2xl border-2 border-sol-white">
+                <input ref={ref} className="rounded-lg m-2 bg-sol-gray-input-hover text-sol-white w-36 lg:w-60" {...props}/>
                 <BiSearchAlt2 className="p-2 text-sol-white h-full w-14 cursor-pointer"/>
             </div>
         </div>
