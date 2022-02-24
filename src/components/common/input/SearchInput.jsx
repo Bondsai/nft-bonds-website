b4a0509af094400a13d83d0c4eb5f6df2a736a4f
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-02-24 16:13:05 +0300

    add mock server

--- src/components/common/input/SearchInput.jsx
+++ src/components/common/input/SearchInput.jsx
@@ -6,7 +6,7 @@ const SearchInput = React.forwardRef((props, ref) => {
         <div className="justify-center flex my-4 p-1">
             <div className="flex justify-center bg-sol-gray-input-hover w-auto rounded-2xl border-2 border-sol-white">
                 <input ref={ref} className="rounded-lg m-2 bg-sol-gray-input-hover text-sol-white w-36 lg:w-60" {...props}/>
-                <BiSearchAlt2 className="p-2 text-sol-white h-full w-14 cursor-pointer"/>
+                <BiSearchAlt2 className="p-2 text-sol-white h-full w-14 cursor-default"/>
             </div>
         </div>
     );
