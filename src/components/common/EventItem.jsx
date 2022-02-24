a8a541a5dba81be52dda4f699e86b8743b14f88c
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-02-24 12:54:29 +0300

    add slider

--- src/components/common/EventItem.jsx
+++ src/components/common/EventItem.jsx
@@ -2,11 +2,16 @@ import React, {useState} from 'react';
 import {BiTimeFive} from "react-icons/bi";
 import {ImCheckmark} from "react-icons/im";
 import {IoMdPricetags} from "react-icons/io";
-import ItemImage from "./image/ItemImage";
 import PurpleBlueButton from "./buttons/PurpleBlueButton";
+import Slider from "./slider/Slider";
 
 const EventItem = ({item}) => {
     const [isFinished, setIsFinished] = useState(item.isFinished)
+    const imgs = [
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+        "https://cnews24.ru/uploads/3f9/3f9a3925a371717aafd19690a2cdd880eb3e256b.png",
+        "https://www.imgonline.com.ua/examples/bee-on-daisy.jpg"
+    ]
 
     const time = "5 days"
     const price = "10 backs"
@@ -17,10 +22,10 @@ const EventItem = ({item}) => {
         <div className="border-solid border-2 border-sol-white rounded-lg w-60 h-100 m-5 transition-shadow">
             <div className="border-solid border-2 border-sol-white rounded-lg h-40 m-5">
                 {isFinished && <div
-                    className="text-2xl justify-center flex absolute w-40 h-10 bg-gradient-to-br from-sol-green to-blue-500 text-white rounded ml-8">
+                    className="z-10 bg-blend-overlay text-2xl justify-center flex absolute w-40 h-10 bg-gradient-to-br from-sol-green to-blue-500 text-white rounded-md ml-[32px]">
                     Collected
                 </div>}
-                <ItemImage isLoading={false}/>
+                <Slider className="z-0" itemsUrls={imgs}/>
             </div>
             <div className="text-sol-white text-xl font-bold truncate hover:text-clip px-5 text-center">
                 {item.name}
---
+++ src/components/common/slider/Slider.jsx
@@ -0,0 +1,39 @@
+import {Swiper, SwiperSlide} from "swiper/react";
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+import React from "react";
+import {Img} from "react-image";
+import SmallLoader from "../loader/SmallLoader";
+import logo from './brokenImgae.png'
+
+
+const Slider = ({props, itemsUrls}) => {
+    SwiperCore.use([Autoplay])
+
+    return (
+        <Swiper
+            props={props}
+            autoplay={{delay: 3000, disableOnInteraction: false}}
+            className="object-cover rounded-lg h-[156px] w-[193px]"
+            slidesPerView={1}
+            centeredSlides
+        >
+            {itemsUrls.map(url =>
+                <SwiperSlide key={url}>
+                    <Img src={url}
+                         className="object-cover rounded-md h-[156px] w-[192px]"
+                         loader={<div className="object-center w-full h-40">
+                             <SmallLoader/>
+                         </div>}
+                         unloader={
+                             <img src={logo} alt="" className="object-center w-full h-40 px-5"/>
+                         }
+                    />
+                </SwiperSlide>
+            )}
+        </Swiper>
+    );
+};
+
+export default Slider;
