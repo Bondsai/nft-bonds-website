ee8a66a2fc36ab3d5167124ac6cd549bb0e6424c
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-02-26 02:30:03 +0300

    fix input

--- src/components/common/input/SearchInput.jsx
+++ src/components/common/input/SearchInput.jsx
@@ -9,7 +9,7 @@ const SearchInput = React.forwardRef((props, ref) => {
                 borderRadius: "18px"
             }}>
                 <input ref={ref}
-                       className="m-2 bg-gray-900 text-sol-white w-36 lg:w-60 outline-0" {...props}
+                       className="m-2 bg-gray-900 text-sol-white w-36 lg:w-60 outline-none" {...props}
                 />
                 <BiSearchAlt2 className="p-2 text-sol-white h-full w-12 cursor-default"/>
             </div>
