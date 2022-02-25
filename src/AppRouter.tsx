5a3f85f7f178ffe376978786f9edfe0474b4a914
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2022-02-25 19:47:46 +0300

    off logo link

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
-import LandingPage from "./pages/LandingPage";
+import {Navigate} from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import EventFetcher from "./pages/event/EventFetcher";
 import ExploreBonds from "./pages/ExploreBonds";
@@ -9,7 +9,7 @@ import CreatePage from "./pages/create/CreatePage";
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={<ExploreBonds/>}/>
+            <Route path="/" element={<Navigate to="/explore"/>}/>
             <Route path="event" element={<EventFetcher/>}/>
             <Route path="create" element={<CreatePage/>}/>
             <Route path="explore" element={<ExploreBonds/>}/>
